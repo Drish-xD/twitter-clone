a4c578b9f02144d7cea4d444dbc362abec4126ed
Drish <drish.xd@gmail.com>
2023-05-11 20:15:09 +0530

    completed boilerplate

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = "twitter_db";
+  static const String projectId = "twitter_clone";
+  static const String endPoint = "https://cloud.appwrite.io/v1";
+}
